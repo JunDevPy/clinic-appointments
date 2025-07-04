a4b162c83caee1368ad35e64b5735d5c64fe729d
JunDevPy <andrejglazov85@gmail.com>
2025-07-04 15:40:33 +0300

    Commit 11 - убрал created_at из models.py пока не нужно это поле

--- api/models.py
+++ api/models.py
@@ -15,7 +15,7 @@ class Appointment(Base):
     patient_name = Column(String, index=True, nullable=False)
     doctor_id = Column(Integer, index=True, nullable=False)
     start_time = Column(DateTime, nullable=False)
-    created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
+    # created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
 
     __table_args__ = (
         UniqueConstraint("doctor_id", "start_time", name="_doctor_time_uc"),
